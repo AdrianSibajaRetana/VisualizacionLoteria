f050a44ca78e495dcf5678f2e6f63bed8bf96889
AdrianSibajaRetana <adriansretana@hotmail.com>
2024-03-28 22:38:31 -0600

    Lista para visualización de tiquetes a jugar.

---
+++ VisualizacionLoteria/Pages/Models/LoteriaComprada.cs
@@ -0,0 +1,13 @@
+﻿namespace VisualizacionLoteria.Pages.Models;
+
+public class LoteriaComprada
+{
+    public int NumeroMayor { get; set; }
+    public int NumeroDeFracciones { get; set; }
+
+    public LoteriaComprada(int NumeroMayor, int NumeroDeFracciones)
+    {
+        this.NumeroMayor = NumeroMayor;
+        this.NumeroDeFracciones = NumeroDeFracciones;
+    }
+}
